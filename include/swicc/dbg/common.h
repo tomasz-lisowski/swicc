797aaed7d23442df6546167f74410e410ae40e29
Tomasz Lisowski <t.p.lisowski@gmail.com>
2022-06-27 18:19:39 +0200

    Add ability to toggle debug colors

--- include/swicc/dbg/common.h
+++ include/swicc/dbg/common.h
@@ -2,6 +2,7 @@
 
 #include "swicc/common.h"
 
+#ifdef DEBUG_CLR
 #define CLR_DEF "\x1B[0m"
 #define CLR_RED "\x1B[31m"
 #define CLR_GRN "\x1B[32m"
@@ -10,6 +11,16 @@
 #define CLR_MAG "\x1B[35m"
 #define CLR_CYN "\x1B[36m"
 #define CLR_WHT "\x1B[37m"
+#else
+#define CLR_DEF ""
+#define CLR_RED ""
+#define CLR_GRN ""
+#define CLR_YEL ""
+#define CLR_BLU ""
+#define CLR_MAG ""
+#define CLR_CYN ""
+#define CLR_WHT ""
+#endif
 
 #define CLR_TXT(clr, txt) clr txt CLR_DEF
 #define CLR_VAL(txt) CLR_CYN txt CLR_DEF
